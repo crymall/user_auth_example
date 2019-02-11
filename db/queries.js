57073d11369e503d6db8cc869f5f8cbe4006628f
Reed Gaines <reed.gaines@gmail.com>
2019-02-11 11:00:38 -0500

    final commit

--- db/queries.js
+++ db/queries.js
@@ -3,24 +3,24 @@ var connectionString = "postgres://localhost/userlist";
 var db = pgp(connectionString);
 
 const authHelpers = require("../auth/helpers");
-const passport = require("../auth/local");
 
 function createUser(req, res, next) {
   const hash = authHelpers.createHash(req.body.password);
 
-  db.none('INSERT INTO users (username, password_digest) VALUES (${username}, ${password})', {username: req.body.username, password: hash})
+  db.none(
+    "INSERT INTO users (username, password_digest) VALUES (${username}, ${password})",
+    { username: req.body.username, password: hash }
+  )
     .then(() => {
-      res.status(200)
-         .json({
-           message: "Registration successful."
-         })
+      res.status(200).json({
+        message: "Registration successful."
+      });
     })
-    .catch((err) => {
-      res.status(500)
-         .json({
-           message: err
-         })
-    })
+    .catch(err => {
+      res.status(500).json({
+        message: err
+      });
+    });
 }
 
 function logoutUser(req, res, next) {
@@ -30,7 +30,7 @@ function logoutUser(req, res, next) {
 
 function loginUser(req, res) {
   res.json(req.user);
-};
+}
 
 module.exports = {
   createUser: createUser,
